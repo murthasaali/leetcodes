9ff0263926955d34fe3ef88abe338346a1455f6b
murthasaali <murthasaalick123@gmail.com>
2024-01-01 18:34:45 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,31 @@
+function palindrome(text){
+    if (typeof text==="string") {
+        const arr =text.split("")
+        const text1=arr.toString()
+        const arr1=arr.reverse().toString()
+        if(text1===arr1)
+        {
+            console.log("palindrom")
+        }
+        else{
+            console.log("not palindrok")
+        } 
+    }else{
+       const d= text.toString()
+        const arr=d.split("")
+        const text1=arr.toString()
+        const arr1=arr.reverse().toString()
+        if(text1===arr1)
+        {
+            console.log("palindrom")
+        }
+        else{
+            console.log("not palindrok")
+        }
+
+    }
+
+    
+}
+text=121
+palindrome(text)
